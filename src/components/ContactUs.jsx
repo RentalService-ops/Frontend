f762e18fffe24944cc8096497741a36b91de2f64
Om Shah <700048@ODCDPW.IC>
2025-03-12 15:16:34 +0530

    Updated Profile and Contact page

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,7 +1,32 @@
+import { useState } from "react"
+import axios from "axios"
 export default function ContactUs() {
+    const [queryData,setQueryData]=useState({
+        username:"",
+        useremail:"",
+        query:""
+    });
 
-    function handleSubmit(){
-        alert("Your query is submitted. It will be resolved at earliest.")
+    async function handleSubmit(){
+        try{
+            await axios.post("http://localhost:8080/contact",queryData,{withCredentials:true})
+            alert("your query is posted. We will resolve it soon.")
+            setQueryData({
+                username:"",
+                useremail:"",
+                query:""
+            })
+        }
+        catch(err){
+            console.log(err.message)
+        }
+    }
+
+    function handleChange(event){
+        const name=event.target.name;
+        const value=event.target.value;
+
+        setQueryData({...queryData,[name]:value})
     }
 
     return (
@@ -10,15 +35,15 @@ export default function ContactUs() {
                 <h2 style={{textAlign:"center"}}>Contact Us</h2>
                 <div className="mb-3">
                     <label for="exampleFormControlInput1" className="form-label">Enter your email address</label>
-                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" name="useremail" value={queryData.useremail} onChange={handleChange}/>
                 </div>
                 <div className="mb-3">
                     <label for="exampleFormControlInput2" className="form-label">Enter your Name</label>
-                    <input type="text" className="form-control" id="exampleFormControlInput2" placeholder="eg- John Doe" />
+                    <input type="text" className="form-control" id="exampleFormControlInput2" placeholder="eg- John Doe" value={queryData.username} name="username" onChange={handleChange}/>
                 </div>
                 <div className="mb-3">
                     <label for="exampleFormControlTextarea1" className="form-label">Enter your message</label>
-                    <textarea className="form-control" id="exampleFormControlTextarea1" placeholder="Short Description of your query" rows="3"></textarea>
+                    <textarea className="form-control" name="query" id="exampleFormControlTextarea1" placeholder="Short Description of your query" rows="3" value={queryData.query} onChange={handleChange}></textarea>
                 </div>
                 <button className="btn btn-primary" onClick={handleSubmit}>Submit</button>
             </div>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,5 +1,170 @@
-export default function Profile(){
-    return(
-        <></>
+import { jwtDecode } from "jwt-decode"
+import { useCookies } from "react-cookie"
+import { useState, useEffect } from "react"
+import axios from "axios"
+
+export default function Profile() {
+
+    const [profileDetails, setProfileDetails] = useState({})
+    const [editProfile, setEditProfile] = useState(false);
+    const [formData,setFormData]=useState({})
+    const [cookie] = useCookies()
+    const role = cookie.role;
+    const userId = jwtDecode(cookie.jwtToken).user_id
+
+    useEffect(() => {
+        async function fetchProfileDetails() {
+            try {
+                const response = await axios.get(`http://localhost:8080/api/${role}/getRenter`, {
+                    params: {
+                        id: `${userId}`
+                    },
+                    headers: {
+                        Authorization: `Bearer ${cookie.jwtToken}`
+                    },
+                    withCredentials: true
+                })
+                setProfileDetails(response.data);
+            }
+            catch (err) {
+                console.log(err.message);
+            }
+        }
+        fetchProfileDetails();
+    }, [])
+
+    function handleChange(event){
+        setFormData({...formData,[event.target.name]:event.target.value})
+    }
+
+    async function handleClick() {
+        for(let key in formData){
+            if(!formData[key]){
+                alert("please fill all the details!!!")
+                return;
+            }
+        }
+        try { 
+            const response=await axios.post(`http://localhost:8080/api/${role}/updateRenter`,formData,{
+                withCredentials:true,
+                headers:{
+                    Authorization:`Bearer ${cookie.jwtToken}`
+                },
+                params:{
+                    id:`${userId}`
+                }
+            })
+            setProfileDetails(response.data);
+            setEditProfile(false);
+        }
+        catch (err) {
+            console.log(err.stack)
+        }
+    }
+
+    return (
+        <>{profileDetails ? <>{!editProfile ?
+            <section style={{ backgroundColor: "#eee", height: "80vh" }}>
+                <div className="row d-flex align-items-center justify-content-center text-center">
+                    <div className="col-lg-8">
+                        <div className="card mb-4">
+                            <div className="card-body">
+                                <div className="row">
+                                    <div className="col-sm-3">
+                                        <p className="mb-0">Full Name</p>
+                                    </div>
+                                    <div className="col-sm-9">
+                                        <p className="text-muted mb-0">{profileDetails.username}</p>
+                                    </div>
+                                </div>
+                                <hr />
+                                <div className="row">
+                                    <div className="col-sm-3">
+                                        <p className="mb-0">Email</p>
+                                    </div>
+                                    <div className="col-sm-9">
+                                        <p className="text-muted mb-0">{profileDetails.email}</p>
+                                    </div>
+                                </div>
+                                <hr />
+                                <div className="row">
+                                    <div className="col-sm-3">
+                                        <p className="mb-0">Phone</p>
+                                    </div>
+                                    <div className="col-sm-9">
+                                        <p className="text-muted mb-0">{profileDetails.phoneNo ||
+                                            "None"}</p>
+                                    </div>
+                                </div>
+                                <hr />
+                                <div className="row">
+                                    <div className="col-sm-3">
+                                        <p className="mb-0">Address</p>
+                                    </div>
+                                    <div className="col-sm-9">
+                                        <p className="text-muted mb-0">{profileDetails.address || "None"}</p>
+                                    </div>
+                                </div>
+                                <div className="d-flex justify-content-center align-items-center mt-3">
+                                    <button className="btn btn-primary" onClick={() =>{ 
+                                    setEditProfile(true);
+                                    setFormData(profileDetails)
+                                    }}>Edit Profile</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section> :
+            <div style={{ height: "80vh" }}>
+                <div className="login-container" >
+                    <div className="mb-3">
+                        <label>Name: </label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="username"
+                            value={formData.username}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="mb-3">
+                    <label>Email: </label>
+                        <input
+                            type="email"
+                            className="form-control"
+                            name="email"
+                            value={formData.email}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="mb-3">
+                    <label>PhoneNo: </label>
+                    <input type="tel" 
+                    className="form-control" 
+                    name="phoneNo" 
+                    value={formData.phoneNo}
+                    placeholder="eg-9999999999"
+                    pattern="[0-9]{10}" 
+                    onChange={handleChange}/>
+                    </div>
+
+                    <div className="mb-3">
+                    <label>Address: </label>
+                    <input type="textarea" 
+                    className="form-control" 
+                    name="address" 
+                    onChange={handleChange} 
+                    value={formData.address}/>
+                    </div>
+                    <div className="d-flex justify-content-between">
+                    <button className="btn btn-primary" onClick={handleClick}>Edit Profile</button>
+                    <button className="btn btn-primary" onClick={()=>setEditProfile(false)}>Go Back</button>
+                    </div>
+                </div>
+            </div>
+        }</>
+            : <>Unable to fetch Details</>}
+        </>
     )
 }
